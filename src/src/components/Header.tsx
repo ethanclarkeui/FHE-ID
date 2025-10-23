f2b2c2bb23f53366319308d2c5a33f12beaac5a7
ethanclarkeui <BusicHilzer674@hotmail.com>
2025-10-23 16:48:19 +0800

    fix

--- src/src/components/Header.tsx
+++ src/src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <div className="header-container">
         <div className="header-content">
           <div className="header-left">
-            <h1 className="header-title">Encrypted Talent Portal</h1>
+            <h1 className="header-title">FHE ID</h1>
             <span className="header-badge">Confidential recruitment</span>
           </div>
           <ConnectButton />
